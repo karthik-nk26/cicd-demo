845fff031ca55b96d94e0abfa8f31a905c0f04e5
Karthik-nk26 <karthiknk826@gmail.com>
2026-02-13 11:42:21 +0000

    push file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working succesfully");
+  res.send("working succesfully");
 });
 
 app.listen(3000, () => {
