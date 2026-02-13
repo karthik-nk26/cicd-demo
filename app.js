7438dc29c1d7b6ca94cd3b7a771788856acd2700
Karthik-nk26 <karthiknk826@gmail.com>
2026-02-13 11:46:09 +0000

    push file final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("working succesfully");
+  res.send("working succesfully, Thank you");
 });
 
 app.listen(3000, () => {
