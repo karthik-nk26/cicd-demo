670678bb9cd92bda9646af80c89db3f1753301b0
Karthik-nk26 <karthiknk826@gmail.com>
2026-02-14 05:12:24 +0000

    New File to congragulate

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("working succesfully, Thank you");
+  res.send("Congragulation");
 });
 
 app.listen(3000, () => {
